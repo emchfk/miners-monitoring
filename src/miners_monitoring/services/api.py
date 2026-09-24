20e484237cbd6902bd1c39ff3e46ecb5112e0e32
emchfk <322185442+emchfk@users.noreply.github.com>
2026-09-24 20:23:23 +0200

    chore: api functions refactoring

--- src/miners_monitoring/services/api.py
+++ src/miners_monitoring/services/api.py
@@ -1,6 +1,6 @@
 import json
 from collections.abc import Awaitable, Callable
-from typing import Any
+from typing import Any, Literal
 
 import httpx
 
@@ -10,69 +10,58 @@ Getter = Callable[[str], Awaitable[Any | str]]
 Poster = Callable[[str], Awaitable[Any | str]]
 
 
+RequestMethod = Literal["get", "post"]  # TODO : patch
+
+
 # Generic function for asynchronously interacting with the API
-async def fetch_data(api_url: str) -> Any | str:
+async def api_interaction(api_url: str, method: RequestMethod = "get") -> Any | str:
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(api_url)
-            response.raise_for_status()  # Check if request was successful (status 200-299)
+            if method == "get":
+                response = await client.get(api_url)
+            elif method == "post":
+                response = await client.post(api_url, headers=None, json=None)
+            else:
+                raise ValueError(f"Unsupported HTTP method: {method}")
 
-            # Display the JSON content of the response
-            return response.json()
-        except httpx.HTTPStatusError as e:
-            return f"HTTP Error : {e.response.status_code} - {e.response.text}"
-        except httpx.RequestError as e:
-            return f"Request Error : {e}"
-
-
-async def post_data(api_url: str) -> Any | str:
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(api_url, headers=None, json=None)
             response.raise_for_status()  # Check if request was successful (status 200-299)
 
             # Check if the response is JSON and return it, otherwise return the text
             try:
                 return response.json()
             except json.JSONDecodeError:
-                print(f"{response.text}")
-                return None
+                return response.text
         except httpx.HTTPStatusError as e:
             return f"HTTP Error : {e.response.status_code} - {e.response.text}"
         except httpx.RequestError as e:
             return f"Request Error : {e}"
 
 
-# GET functions for specific API endpoints
-async def get_system_data(ip: str, endpoint: str) -> Any | str:
+async def request_system_data(
+    ip: str, endpoint: str, method: RequestMethod = "get"
+) -> Any | str:
     url = url_template.format(ip, endpoint)
-    return await fetch_data(url)
+    return await api_interaction(url, method=method)
 
 
 def make_getter(endpoint: str) -> Getter:
     async def getter(ip: str) -> Any | str:
-        return await get_system_data(ip, endpoint)
+        return await request_system_data(ip, endpoint, "get")
 
     return getter
+
+
+def make_poster(endpoint: str) -> Poster:
+    async def poster(ip: str) -> Any | str:
+        return await request_system_data(ip, endpoint, "post")
+
+    return poster
 
 
 get_system_info = make_getter("info")
 get_asic_settings_info = make_getter("asic")
 get_system_statistics = make_getter("statistics")
 get_wifi_scan = make_getter("wifi/scan")
-
-
-# POST functions for specific API endpoints
-async def post_system_data(ip: str, endpoint: str) -> Any | str:
-    url = url_template.format(ip, endpoint)
-    return await post_data(url)
-
-
-def make_poster(endpoint: str) -> Poster:
-    async def poster(ip: str) -> Any | str:
-        return await post_system_data(ip, endpoint)
-
-    return poster
 
 
 post_restart = make_poster("restart")
